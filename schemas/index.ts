dc4c560f7fea3dd97f6333b994c25de6d9621c8b
Zekima <christianledaal@gmail.com>
2024-02-12 22:53:46 +0100

    fix: Allow google profile pictures domain

--- schemas/index.ts
+++ schemas/index.ts
@@ -2,21 +2,21 @@ import * as z from "zod";
 
 export const LoginSchema = z.object({
   email: z.string().email({
-    message: "Email is required",
+    message: "Email er obligatorisk",
   }),
   password: z.string().min(1, {
-    message: "Password is required",
+    message: "Passord er obligatorisk",
   }),
 });
 
 export const RegisterSchema = z.object({
   email: z.string().email({
-    message: "Email is required",
+    message: "Email er obligatorisk",
   }),
   name: z.string().min(1, {
-    message: "Name is required",
+    message: "Navn er obligatorisk",
   }),
   password: z.string().min(6, {
-    message: "Minimum 6 characters required",
+    message: "Minimum 6 bokstaver",
   }),
 });
