1b29f536984d92c0035d807dcd27acb3265d29a2
Zekima <christianledaal@gmail.com>
2024-02-12 23:47:17 +0100

    placeholder icons

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -14,27 +14,39 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
+import { IoIosArrowDropdownCircle, IoIosNotifications } from "react-icons/io";
+import { BsChatSquareTextFill } from "react-icons/bs";
+
 const NavBar = () => {
   const session = useSession();
   const user = session.data?.user;
 
-  const userProfileHref = `/profil/${user?.id}`
+  const userProfileHref = `/profil/${user?.id}`;
 
   return (
-    <div className="flex py-5 h-16 2xl:px-0 justify-between m-auto max-w-[1280px] items-center">
+    <div className="flex py-5 h-16 px-5 2xl:px-0 justify-between m-auto max-w-[1280px] items-center">
       <Link href="/">
         <h1 className="font-bold text-xl">DugnadNett</h1>
       </Link>
       {user ? (
         <>
-          <div className="flex items-center">
-            <p className="mr-3 font-medium">{user.name}</p>
+          <div className="flex items-center gap-3.5">
+            <BsChatSquareTextFill size={24}/>
+            <IoIosNotifications size={24} />
 
             <DropdownMenu>
-              <DropdownMenuTrigger className="flex items-center select-none">
+              <DropdownMenuTrigger className="flex items-center select-none relative">
+                <IoIosArrowDropdownCircle className="absolute right-0 bottom-0" />
                 <UserImage user={user} />
               </DropdownMenuTrigger>
-              <DropdownMenuContent className="" onCloseAutoFocus={(e) => e.preventDefault()}>
+              <DropdownMenuContent
+                className="bg-white"
+                onCloseAutoFocus={(e) => e.preventDefault()}
+              >
+                <p className="mt-1 font-medium text-xs text-center w-36">
+                  {user.name}
+                </p>
+                <DropdownMenuSeparator className="border border-muted" />
                 <Link href={userProfileHref} passHref legacyBehavior>
                   <DropdownMenuItem className="cursor-pointer hover:bg-gray-200">
                     <MdAccountCircle className="mr-2 h-4 w-4" />
@@ -47,8 +59,9 @@ const NavBar = () => {
                     Innstillinger
                   </DropdownMenuItem>
                 </Link>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem className="cursor-pointer hover:bg-gray-200"
+                <DropdownMenuSeparator className="border border-muted" />
+                <DropdownMenuItem
+                  className="cursor-pointer hover:bg-gray-200"
                   onClick={async () => {
                     await signOut();
                   }}
