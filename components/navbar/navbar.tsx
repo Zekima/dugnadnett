bf6f2e79edd169261bcd99614d291943cf64df55
Zekima <christianledaal@gmail.com>
2024-02-13 09:22:29 +0100

    Outlined icons

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -14,8 +14,9 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-import { IoIosArrowDropdownCircle, IoIosNotifications } from "react-icons/io";
-import { BsChatSquareTextFill } from "react-icons/bs";
+import { IoIosArrowDropdownCircle } from "react-icons/io";
+import { IoNotificationsOutline } from "react-icons/io5";
+import { IoChatbubbleOutline } from "react-icons/io5";
 
 const NavBar = () => {
   const session = useSession();
@@ -24,58 +25,61 @@ const NavBar = () => {
   const userProfileHref = `/profil/${user?.id}`;
 
   return (
-    <div className="flex py-5 h-16 px-5 2xl:px-0 justify-between m-auto max-w-[1280px] items-center">
-      <Link href="/">
-        <h1 className="font-bold text-xl">DugnadNett</h1>
-      </Link>
-      {user ? (
-        <>
-          <div className="flex items-center gap-3.5">
-            <BsChatSquareTextFill size={24}/>
-            <IoIosNotifications size={24} />
+    <div className="border-b-2 border-gray">
+      <div className="flex py-5 h-16 px-5 2xl:px-0 justify-between m-auto max-w-[1280px] items-center">
+        <Link href="/">
+          <h1 className="font-bold text-xl">DugnadNett</h1>
+        </Link>
+        {user ? (
+          <>
+            <div className="flex items-center gap-3.5">
+              <IoChatbubbleOutline size={26} />
 
-            <DropdownMenu>
-              <DropdownMenuTrigger className="flex items-center select-none relative">
-                <IoIosArrowDropdownCircle className="absolute right-0 bottom-0" />
-                <UserImage user={user} />
-              </DropdownMenuTrigger>
-              <DropdownMenuContent
-                className="bg-white"
-                onCloseAutoFocus={(e) => e.preventDefault()}
-              >
-                <p className="mt-1 font-medium text-xs text-center w-36">
-                  {user.name}
-                </p>
-                <DropdownMenuSeparator className="border border-muted" />
-                <Link href={userProfileHref} passHref legacyBehavior>
-                  <DropdownMenuItem className="cursor-pointer hover:bg-gray-200">
-                    <MdAccountCircle className="mr-2 h-4 w-4" />
-                    Profil
-                  </DropdownMenuItem>
-                </Link>
-                <Link href="/innstillinger">
-                  <DropdownMenuItem className="cursor-pointer hover:bg-gray-200">
-                    <MdSettings className="mr-2 h-4 w-4" />
-                    Innstillinger
-                  </DropdownMenuItem>
-                </Link>
-                <DropdownMenuSeparator className="border border-muted" />
-                <DropdownMenuItem
-                  className="cursor-pointer hover:bg-gray-200"
-                  onClick={async () => {
-                    await signOut();
-                  }}
+              <IoNotificationsOutline size={26} />
+
+              <DropdownMenu>
+                <DropdownMenuTrigger className="flex items-center select-none relative">
+                  <IoIosArrowDropdownCircle className="absolute right-0 bottom-0 bg-white rounded-xl" />
+                  <UserImage user={user} />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent
+                  className="bg-white"
+                  onCloseAutoFocus={(e) => e.preventDefault()}
                 >
-                  <MdLogout className="mr-2 h-4 w-4" />
-                  Logout
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-        </>
-      ) : (
-        <Link href="/auth/login">Login/Register</Link>
-      )}
+                  <p className="mt-1 font-medium text-xs text-center w-36">
+                    {user.name}
+                  </p>
+                  <DropdownMenuSeparator className="border border-muted" />
+                  <Link href={userProfileHref} passHref legacyBehavior>
+                    <DropdownMenuItem className="cursor-pointer hover:bg-gray-200">
+                      <MdAccountCircle className="mr-2 h-4 w-4" />
+                      Profil
+                    </DropdownMenuItem>
+                  </Link>
+                  <Link href="/innstillinger">
+                    <DropdownMenuItem className="cursor-pointer hover:bg-gray-200">
+                      <MdSettings className="mr-2 h-4 w-4" />
+                      Innstillinger
+                    </DropdownMenuItem>
+                  </Link>
+                  <DropdownMenuSeparator className="border border-muted" />
+                  <DropdownMenuItem
+                    className="cursor-pointer hover:bg-gray-200"
+                    onClick={async () => {
+                      await signOut();
+                    }}
+                  >
+                    <MdLogout className="mr-2 h-4 w-4" />
+                    Logout
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </>
+        ) : (
+          <Link href="/auth/login">Login/Register</Link>
+        )}
+      </div>
     </div>
   );
 };
