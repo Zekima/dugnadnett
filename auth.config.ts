6e6a84563633ddbe447838165db485a3c7e98ea9
Zekima <christianledaal@gmail.com>
2024-03-02 06:37:41 +0100

    fix: trustHost set to true for production

--- auth.config.ts
+++ auth.config.ts
@@ -1,44 +1,45 @@
-import Credentials from "next-auth/providers/credentials"
-import Github from 'next-auth/providers/github'
-import Google from 'next-auth/providers/google'
-import Facebook from 'next-auth/providers/facebook'
-import type { NextAuthConfig } from "next-auth"
-import bcrypt from "bcryptjs"
+import Credentials from "next-auth/providers/credentials";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import Facebook from "next-auth/providers/facebook";
+import type { NextAuthConfig } from "next-auth";
+import bcrypt from "bcryptjs";
 
-import { LoginSchema } from "./schemas"
-import { getUserByEmail } from './data/user'
+import { LoginSchema } from "./schemas";
+import { getUserByEmail } from "./data/user";
 
 export default {
-    providers: [
-      Github({
-        clientId: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET
-      }),
-      Google({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET
-      }),
-      Facebook({
-        clientId: process.env.FACEBOOK_CLIENT_ID,
-        clientSecret: process.env.FACEBOOK_CLIENT_SECRET
-      }),
+  trustHost: true,
+  providers: [
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Facebook({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    }),
 
-      Credentials({
-        async authorize(credentials) {
-            const validatedFields = LoginSchema.safeParse(credentials);
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
 
-            if (validatedFields.success) {
-                const {email, password} = validatedFields.data;
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
 
-                const user = await getUserByEmail(email)
-                if (!user || !user.password) return null;
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) return null;
 
-                const passwordsMatch = await bcrypt.compare(password, user.password);
+          const passwordsMatch = await bcrypt.compare(password, user.password);
 
-                if (passwordsMatch) return user;
-            }
-            return null;
+          if (passwordsMatch) return user;
         }
-    })
-    ],
-} satisfies NextAuthConfig
+        return null;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
