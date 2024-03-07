cf8f03db1922af8efc2764473cd19ba9664105d2
KristianVeggeland <kristianov2000@hotmail.com>
2024-03-07 15:11:21 +0100

    Fixed some of the desing of the FAQ page

--- components/misc/faq/faq-card.tsx
+++ components/misc/faq/faq-card.tsx
@@ -1,29 +1,27 @@
-"use client"
-import React, { useState} from "react";
-import {Faq} from "@/types";
-const FaqCard = ({faq} : {faq?: Faq}) => {
+"use client";
+import React, { useState } from "react";
+import { Faq } from "@/types";
+const FaqCard = ({ faq }: { faq?: Faq }) => {
+  const [extend, setExtend] = useState(false);
 
-    const [extend, setExtend] = useState(false);
+  function handleExtend() {
+    setExtend(!extend);
+  }
 
-    function handleExtend() {
-        setExtend(!extend);
-    }
+  if (!faq) return;
 
-    if (!faq) return;
-
-    return (
-        <div>
-            <div className="">
-                <h2>
-                    {faq.title}
-                </h2>
-                <button onClick={handleExtend}> {!extend ? '+' : '-' } </button>
-            </div>
-                {extend && (
-                    <div> {faq.text} </div>
-                    )}
-                </div>
-    )
-}
+  return (
+    <div className="flex-basis-0.5 flex-column flex-gap-5 place-items-center justify-center">
+      <div className="flex-gap-10 flex flex-row ">
+        <h2 className="text-lg font-bold">{faq.title}</h2>
+        <button className="" onClick={handleExtend}>
+          {" "}
+          {!extend ? "+" : "-"}{" "}
+        </button>
+      </div>
+      {extend && <p className="mb-2 text-lg"> {faq.text} </p>}
+    </div>
+  );
+};
 
 export default FaqCard;
