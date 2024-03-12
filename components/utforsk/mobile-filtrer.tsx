1350abd870831116c5370e1a866835db7caba46d
Zekima <christianledaal@gmail.com>
2024-03-12 23:41:46 +0100

    add mobile drawer close

--- components/utforsk/mobile-filtrer.tsx
+++ components/utforsk/mobile-filtrer.tsx
@@ -6,13 +6,16 @@ import {
     Drawer,
     DrawerContent,
     DrawerTrigger,
+    DrawerHeader
 } from "@/components/ui/drawer"
 
 const MobileFiltrer = (props: any) => {
     return (
         <Drawer>
             <DrawerTrigger className="py-1 px-2 rounded-md text-sm bg-white border-2 hover:bg-gray-100">Filtrer</DrawerTrigger>
-            <DrawerContent className="bg-white h-5/6 p-5">
+            <DrawerContent className="bg-white h-full p-5">
+                <div className="flex items-center justify-center">
+                <div className="bg-gray-200 h-2 mb-6 rounded-lg w-1/2"></div></div>
                 <Filtrer {...props} />
             </DrawerContent>
         </Drawer>
