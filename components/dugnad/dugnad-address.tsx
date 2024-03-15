82b70561ba3a82acc5ade68e978ecfdcdfd3a243
Zekima <christianledaal@gmail.com>
2024-03-15 23:30:12 +0100

    Add copy to clipboard for address

--- components/dugnad/dugnad-address.tsx
+++ components/dugnad/dugnad-address.tsx
@@ -1,9 +1,24 @@
+'use client'
+
 import React from "react";
 import { Map } from "lucide-react";
+import { Check } from "lucide-react";
+import { useToast } from "@/components/ui/use-toast"
 
 const DugnadAddress = ({address}: any) => {
+    const { toast } = useToast()
+
+
+    const handleShare = () => {
+        navigator.clipboard.writeText(`${window.location}`)
+        toast({
+            className: "bg-green-800 border-none",
+            description: <div className='flex gap-3 items-center text-white font-bold'><Check /> <p>Adressen er kopiert til utklippstavlen</p></div>,
+            duration: 1500,
+        })
+    }
     return (
-        <div className="flex items-center gap-1.5 mt-2 bg-gray-200 p-3 rounded-md mb-2"><Map /> <p>{address}</p></div>
+        <div onClick={() => handleShare()} className="flex items-center gap-1.5 mt-2 bg-gray-200 p-3 rounded-md mb-2 cursor-pointer"><Map /> <p>{address}</p></div>
     );
 }
 
