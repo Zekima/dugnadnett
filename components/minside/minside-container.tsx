52536d5517d12eb4411e22469f8acf29704ab63e
Kristian Veggeland <kristianov2000@hotmail.com>
2024-03-15 13:10:44 +0100

    fixing the design of minside

--- components/minside/minside-container.tsx
+++ components/minside/minside-container.tsx
@@ -9,23 +9,29 @@ const MinsideContainer = async () => {
     const ownesDugnads = await getUserOwnesDugnads();
     const partDugnads = await getUserParticpatesInDugnads();
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 lg:grid-cols-3">
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 lg:grid-cols-3">
-            {ownesDugnads.map((dugnad) => (
-                <MinsideCard dugnad={dugnad} key={dugnad.id} /> 
-            )
-            )}
-            
-        </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 lg:grid-cols-3">
-         {partDugnads.map((dugnad) => (
-             <MinsideCard dugnad={dugnad} key={dugnad.id}/> 
-         )
-         )}
-         
+        <div>
+             <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 lg:grid-cols-3">
+                <h1 className="text-2xl font-bold mb-3 sm: pt-[40px] pt -[40px]">Username:</h1>
+                <div className="grid grid-cols-1 sm:grid-cols-1 gap-1 lg:grid-cols-2">
+                    <h2 className="text-2xl font-bold mb-3 sm:pt-[100px] pt-[10px]">mine:</h2>
+                    {ownesDugnads.map((dugnad) => (
+                        <MinsideCard dugnad={dugnad} key={dugnad.id} /> 
+                    )
+                    )}
+                
+                </div>
+                <div className="grid grid-cols-1 sm:grid-cols-1 gap-1 lg:grid-cols-2">
+                    <h2 className="text-2xl font-bold mb-3 sm:pt-[100px] pt-[10px]">mine:</h2>
+                    {partDugnads.map((dugnad) => (
+                        <MinsideCard dugnad={dugnad} key={dugnad.id} /> 
+                    )
+                    )}
+                
+                </div>
+                
             </div>
         </div>
-        
+    
     )
 }
 
