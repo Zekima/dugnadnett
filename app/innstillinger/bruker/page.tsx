dd36cccbf848224dc2b0e77cb410d0293254e5a7
zekima <christianledaal@gmail.com>
2024-04-28 17:58:42 +0200

    Improve profile settings

--- app/innstillinger/bruker/page.tsx
+++ app/innstillinger/bruker/page.tsx
@@ -2,19 +2,16 @@ import { Separator } from "@/components/ui/separator";
 import UserImage from "@/components/user-image";
 import { getCurrentUser } from "@/lib/auth";
 
-
 const UserSettingsPage = async () => {
-    const user = await getCurrentUser();
+  const user = await getCurrentUser();
 
-    return (
-        <div>
-        <h1 className="text-xl font-medium">Brukerinnstillinger</h1>
-        <Separator className="my-3" />
-        <div className="flex flex-col gap-6">
-          </div>
-      </div>
-    )
-}
-
+  return (
+    <div>
+      <h1 className="text-2xl font-medium">Brukerinnstillinger</h1>
+      <Separator className="my-6" />
+      <div className="flex flex-col gap-6"></div>
+    </div>
+  );
+};
 
 export default UserSettingsPage;
--- app/innstillinger/page.tsx
+++ app/innstillinger/page.tsx
@@ -5,6 +5,7 @@ import { Separator } from "@/components/ui/separator";
 import UserImage from "@/components/user-image";
 import { getUserById } from "@/data/user";
 import { getCurrentUser } from "@/lib/auth";
+import {Pencil, Trash} from 'lucide-react'
 
 const SettingsPage = async () => {
   const user = await getCurrentUser();
@@ -19,17 +20,20 @@ const SettingsPage = async () => {
 
   return (
     <div>
-      <h1 className="text-xl font-medium">Min profil</h1>
-      <Separator className="my-3" />
+      <h1 className="text-2xl font-medium">Profilinnstillinger</h1>
+      <Separator className="my-6" />
       <div className="flex flex-col gap-6">
-        <div className="mt-3 mb-2 flex gap-6">
+      <h2 className="font-medium">Profilbilde</h2>
+
+        <div className="mb-2 flex gap-6">
+          
           <UserImage user={user} size={128} />
-          <div className="flex flex-col gap-1 justify-center">
-            <button className="px-4 py-2 rounded-md bg-green-700 text-white hover:bg-green-800">
-              Endre profilbilde
+          <div className="flex flex-col gap-2 justify-center">
+            <button className="px-4 flex items-center gap-2 py-2 rounded-md bg-green-700 text-white hover:bg-green-800">
+              <Pencil size={20}/> Endre
             </button>
-            <button className="px-4 py-2 rounded-md bg-black text-white hover:bg-gray-800">
-              Fjern profilbilde
+            <button className="px-4 flex items-center gap-2 py-2 rounded-md bg-red-700 text-white hover:bg-red-800">
+              <Trash size={20}/> Fjern
             </button>
           </div>
         </div>
